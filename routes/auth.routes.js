b667d88c2a07ec5e1cb3f4a58afeb534cb611fff
Liliia Khrystiuk <liliya.khrystyuk@gmail.com>
2021-07-22 22:21:52 +0200

    bugs fixed

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,33 +4,26 @@ const bcrypt = require('bcryptjs');
 
 router.post('/auth/signup', async (req,res, next) => {
     const {username, email, password, image_url, description} = req.body;
-
     if (!username || !email || !password) {
         res.status(500).json({errorMessage: "Please fill in all fields"});
         return;
     }
-    
     const emailreg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!emailreg.test(email)) {
         res.status(500).json({errorMessage: "Please enter a valid email"});
         return;
     }
-
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt)
-    
     let passcheck = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,14}$/
     if(!passcheck.test(password)){
-
         res.status(500).json({errorMessage: `Password must have:
         - At least one number;
         - At least one special characters;
         - Must be between 6 and 14 characters;
         `});
-
     return;
     }
-
     try {
         let userData = await UserModel.create({username, email, password: hash, image_url, description})
         userData.password = '***'
@@ -72,12 +65,12 @@ router.post('/auth/login', async (req, res, next) => {
     catch (error) {
        res.status(500).json({errorMessage: 'Username does not exist'})
     }
-})
+});
 
 router.post('/auth/logout', (req,res) => {
     req.session.destroy();
     res.status(204).json({successMessage: 'Thank you, see you next time'})
-})
+});
 
 const isLoggedIn = (req, res, next) => {
     if (req.session.loggedInUser) {
@@ -88,10 +81,10 @@ const isLoggedIn = (req, res, next) => {
           errorMessage: 'Not an authorized user'
         })
     }
-}
+};
 
 router.get('/user', isLoggedIn, (req,res,next) => {
     res.status(200).json(req.session.loggedInUser);
-})
+});
 
 module.exports = router;
