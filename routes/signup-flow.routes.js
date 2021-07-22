0ea5f57babe3ec130991d58d7ffc7d71d3b4d592
Liliia Khrystiuk <liliya.khrystyuk@gmail.com>
2021-07-22 22:00:43 +0200

    bug fixes

--- routes/signup-flow.routes.js
+++ routes/signup-flow.routes.js
@@ -1,18 +1,19 @@
 const router = require("express").Router();
 const UserModel = require('../models/User.model');
 
+
 // NEED TO BE TESTED
 router.post('/signup/category', async (req, res, next) => {
     
     const { _id, categories } = req.body;
 
     try {
-        let response = await UserModel.findByIdAndUpdate(_id, {categories}, {new: true})
+        let response = await UserModel.findByIdAndUpdate(_id, {categories: categories})
         res.status(200).json(response)
     }
     catch(error) {
         res.status(500).json({
-            errorMessage: "Hmm, are you sure you? Please try again!"
+            errorMessage: "Hmm, are you sure you? Pleas try again!"
         })
     }
 });
