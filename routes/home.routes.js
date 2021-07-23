a0ca51d714229588dd9065d58569b35bcdfd65db
Liliia Khrystiuk <liliya.khrystyuk@gmail.com>
2021-07-23 19:04:58 +0200

    Friday evening commit from lily. Minor changes in home.routes.

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -2,16 +2,18 @@ const express = require('express');
 const router = express.Router();
 const PostModel =  require('../models/Post.model');
 
+
+
 router.get('/home', async (req, res) => {
 
     try{
-        let response = await PostModel.find();
+        let response = await PostModel.find().limit(10);
         res.status(200).json("Home page", response)
     }
     catch(error){
         res.status(500).json({
             errorMessage: "Post cannot be loaded",
-            message: err
+            message: error
         })
     }
 })
