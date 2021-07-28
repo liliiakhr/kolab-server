11b644e9a4e2d0611d004de249d5073543f032c7
Liliia Khrystiuk <liliya.khrystyuk@gmail.com>
2021-07-28 12:13:19 +0200

    push before deploy

--- app.js
+++ app.js
@@ -62,6 +62,9 @@ app.use("/api", postRoutes);
 const friendRoutes = require('./routes/friends.routes');
 app.use("/api", friendRoutes);
 
+const chatRoutes = require('./routes/chat.routes');
+app.use("/api", chatRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
