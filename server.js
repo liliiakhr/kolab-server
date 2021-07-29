c3acbd989604a0120231441fbc3d90179cacea67
Kalande Mwamba <kalandekupe@outlook.com>
2021-07-29 09:18:36 +0200

    Video chat v2

--- server.js
+++ server.js
@@ -44,5 +44,15 @@ io.on('connection', (socket) => {
       })
     
   });
+
+  socket.emit('me', socket.id)
+
+  socket.on("callUser", (data) => {
+     io.to(data.userToCall).emit("callUser", {signal: data.signalData, from: data.from, name: data.name})
+  })
+
+  socket.on("answerCall", (data) => {
+     io.to(data.to).emit("callAccepted", data.signal)
+  })
 });
 
